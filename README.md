42d5d7ff643cf64a60b78840f02040efa620b9fe
olneykimberly <olneykimberly@gmail.com>
2025-11-19 16:02:15 -0700

    updated README & DE with excluded samples

--- README.md
+++ README.md
@@ -1,17 +1,67 @@
 # chemobrain
 The transcriptional alternations within the brain associated with chemo therapy 
 
+| Group                     | Count   | Sex (F, M)|
+| ------------------------- |:-------:|:-------:|
+| SALINE                    | 12      | (6,6)   |
+| LEUC                      | 8       | (4,4)   |
+| MTX                       | 8       | (5,3)   |
+| MTXLEUC                   | 14      | (5,9)   |
 
-#---
-01_processing
-processing raw fastq files
+This git repo contains scripts for the following:
+-   Metadata analysis
+-   Processing of bulk RNA-sequencing data
+-   Generation of manuscript figures 
+-   Generation of shiny app for exploration of the results, view app [here]( https://fryerlab.shinyapps.io/chemobrain_mice/)
+
+
+## Set up conda environment
+This workflow uses conda. For information on how to install conda [here](https://docs.conda.io/projects/conda/en/latest/user-guide/index.html)
+
+To create the conda environment:
 ```
-cd 01_processing/
-sh 01_get_readgroup_info.sh
-python 02_create_config.py
+conda env create -n chemobrain --file chemobrain.yml
+
+# To activate this environment, use
+#
+#     $ conda activate chemobrain
+#
+# To deactivate an active environment, use
+#
+#     $ conda deactivate chemobrain
 ```
 
-# snakemake 
+## Reference genome
+Reference genome and annotation were downloaded prior to running snakemake. 
+Ensembl refdata-gex-GRCm39-2024-A fasta
+
 ```
-sbatch 03_run_snakemake.sh
+wget https://cf.10xgenomics.com/supp/cell-exp/refdata-gex-GRCm39-2024-A.tar.gz
+STAR --runMode genomeGenerate --runThreadN 8 --genomeDir refdata-gex-GRCm39-2024-A_STARv2.7.11_150sjdb  --genomeFastaFiles genome.fa --sjdbGTFfile genes.gtf --sjdbOverhang 150
 ```
+
+## Snakemake for trimming reads and alignment to the reference genome
+See the snakefile for specific details for each step. 
+The config file was generated using the 01 and 02 scripts for obtaining sample information and creating the config file. 
+```
+Snakemake -s Snakefile 
+```
+Output includes STAR reads per gene, which can then be read into R for differential expression.
+
+## Variance assessment and differential expression
+```
+R 04_differential_expression.Rmd
+```
+Output is differentially expressed genes for all pairwise comparisons.
+
+## References
+All packages used in this workflow are publicly available. If you use this workflow please cite the packages used. 
+If you use the data in this workflow cite the following:
+[et al. 2026]()
+
+## Contacts
+
+| Contact | Email |
+| --- | --- |
+| Kimberly Olney, PhD | kolney@tgen.org |
+| John Fryer, PhD | jfryer@tgen.org |
--- scripts/file_paths_and_colours.R
+++ scripts/file_paths_and_colours.R
@@ -10,7 +10,7 @@ library(devtools)
 library(reshape2)
 library(edgeR)  
 library(limma)  
-library(tximport)
+#library(tximport)
 library(tidyverse)
 library(GenomicFeatures)
 library(data.table)
@@ -18,6 +18,7 @@ library(philentropy)
 library(gplots)
 library(variancePartition)
 library(NatParksPalettes) # colors
+library(ComplexUpset)
 
 
 #BiocManager::install("variancePartition", lib="/tgen_labs/jfryer/YOURACCOUNTHERE/R/x86_64-pc-linux-gnu-library/4.3")
@@ -36,14 +37,68 @@ saveToPDF <- function(...) {
   dev.off(d)
 }
 
+### --- Function to Extract Gene Expression ---
+# Helper function to extract CPM for a specific gene and merge it into the info dataframe
+extract_gene_cpm <- function(gene_name, cpm_data, info_data) {
+  # Subset for the specific gene
+  gene_cpm <- cpm_data %>%
+    dplyr::filter(gene_name == !!gene_name)
+  
+  # Melt and clean the data
+  gene_cpm_melt <- gene_cpm %>%
+    reshape2::melt() %>%
+    dplyr::rename(sample = variable) %>%
+    dplyr::filter(sample != "gene_name") %>% # Remove the gene_name column row
+    dplyr::select(sample, value) %>%
+    dplyr::rename(!!gene_name := value)
+  
+  # Merge the new gene column into the info dataframe
+  info_data <- info_data %>%
+    dplyr::left_join(gene_cpm_melt, by = "sample")
+  
+  return(info_data)
+}
+
+fromList <- function (input) {
+  # Same as original fromList()...
+  elements <- unique(unlist(input))
+  data <- unlist(lapply(input, function(x) {
+    x <- as.vector(match(elements, x))
+  }))
+  data[is.na(data)] <- as.integer(0)
+  data[data != 0] <- as.integer(1)
+  data <- data.frame(matrix(data, ncol = length(input), byrow = F))
+  data <- data[which(rowSums(data) != 0), ]
+  names(data) <- names(input)
+  # ... Except now it conserves your original value names!
+  row.names(data) <- elements
+  return(data)
+}
+
+makePaddedDataFrame <- function(l, ...) {
+  maxlen <- max(sapply(l, length))
+  data.frame(lapply(l, na.pad, len = maxlen), ...)
+}
 #----------------- Data
-metadata_1MPI <- read.delim("/tgen_labs/jfryer/kolney/chemobrain/metadata_1MPI.tsv", header = TRUE, sep = "\t")
-metadata_1WPI <- read.delim("/tgen_labs/jfryer/kolney/chemobrain/metadata_1WPI.tsv", header = TRUE, sep = "\t")
-metadata_1WPI <- metadata_1WPI %>%
+#metadata_1MPI <- read.delim("/tgen_labs/jfryer/kolney/chemobrain/metadata_1MPI.tsv", header = TRUE, sep = "\t")
+metadata <- read.delim("/tgen_labs/jfryer/kolney/chemobrain/metadata_1WPI.tsv", header = TRUE, sep = "\t")
+metadata <- metadata %>%
   mutate(Group = gsub("MTXL", "MTXLEUC", Group)) # Why all of a sudden is it MTXL?! convert back to MTXLEUC to be consistent. 
 
 # Update Sample_ID
 # metadata$Sample_ID <- sub("MTX_LEUC", "MTXLEUC", metadata$Sample_ID)
 
 # Exclude samples 
-# metadata <- metadata[metadata$Study_Specimen_ID != "CBP_16_F_Saline",] #
+# "F9_SALINE", "M5_SALINE", "M3_SALINE", "M1_LEUC", "F1_SALINE", "F5_LEUC", "F4_MTX", "F1_MTX", "F6_MTXL", "M12_MTXL"
+# Define the list of Sample_IDs to be removed
+samples_to_remove <- c(
+  "F9_SALINE", "M5_SALINE", "M3_SALINE", "M1_LEUC", "F1_SALINE",
+  "F5_LEUC", "F4_MTX", "F1_MTX", "F6_MTXL", "M12_MTXL", "F4_LEUC", "M7_SALINE", "M4_MTX", "M2_MTX", "M10_MTXL", "F4_MTXL", "F8_MTXL", 
+  "M11_SALINE", "M1_SALINE", "M2_SALINE", "F10_SALINE", "M11_MTXL", "F8_SALINE", "F2_SALINE", "F7_MTXL"
+)
+
+# Filter the metadata data frame to remove the specified samples 
+# AND the existing condition for "CBP_16_F_Saline"
+metadata <- metadata[
+  !(metadata$Sample_ID %in% samples_to_remove),
+]
